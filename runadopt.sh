3184ef65319e060d32c613e47d68b75b099f86cf
Mickael Asseline <papamica@PAPAMICA-PCPRO.localdomain>
2021-12-10 01:56:02 +0100

    Add Zabbix agent

--- runadopt.sh
+++ runadopt.sh
@@ -24,3 +24,13 @@ curl --user 'webdav:webdav1309' -T "$NODENAME.yml" 'http://nurion.papamica.com:1
 echo "    => Inventory file sended !"
 echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCqRxD6h9I3+eVB6kEbAZ3pqBJO5EpWQr/hE0w/pxPYQ6dRC9FlvYJIkvEttnTWGwAaTzW/ysoXsgsJnCDOxG9QTYI1lA+2WhTTDxfYUHD1SKPua6nvrQdu4Z9+8cz5EqR5ppzWUH0lT//0aaYAuvFDuAzicxC4d+Y+9Hik8BgVDG3vXficQCtriRcWaFrKscIkYQKi6PRqMljFeAFVlfvrELkWC38JHhdcTwO6X3SCyGlcqvdkaqhKTb+61ZkiReSVARSB6CswdsG1XmTRFdbBm1WNeCmYhOPDvyEri/E7vY4WvqT5JFrcryMWPVHwE8yjQ5DudpPX5PT6GeABf/MZ3cVJPQ+Wy5/TZLSq4/jkS6UffAM7nLGvSEwWHLoMZRIhF6Z9vrc5pfGFI4pf7lm6z08Tk81s/wkcOw7nZzxElJvFlGq3EI6oCt6GgS92hamoCHwUl7hXFwIvKCfc8Cehsx5pSN8gmOd+l4SdRZsu+fnltlEpLPY97Ul7BAvBHXU= root@NURION.PAPAMICA.COM #RunDeck" >> ~/.ssh/authorized_keys
 echo "    => SSH Key added !"
+echo ""
+read -p "  Install Zabbix agent ? (Y/n) " installzabbix
+
+  if [[ "$installzabbix" == "n" ]]; then
+        exit
+    else
+        JSONDATA=$(echo "'{\"field1\":\"$NODENAME\"}'")
+        curl -H "Content-Type: application/json" -X POST -d $JSONDATA https://rundeck.papamica.com/api/40/webhook/U1mWyNjTEKJc6ZjpGF4Q3msb4FIsR7Gb#New_Server_%3E_Agent_Zabbix
+        echo "    => Zabbix Agent install started !"
+  fi
